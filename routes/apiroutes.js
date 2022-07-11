63976a92155814a83ad397483503569c9dd3ee1a
Tanya Milburn <tanyamilburn89@gmail.com>
2022-07-10 18:05:03 -0600

    tried to link notes.html

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,19 +1,28 @@
 const router = require('express').Router()
 const path = require('path');
 const fs = require('fs');
+
+function getNotes(){
+    const data= fs.readFileSync(path.join(__dirname,"../db/db.json"), "utf-8");
+    const parsedData = JSON.parse(data).notes;
+    console.log("parsedData", parsedData)
+    return parsedData
+}
+
 router.get('/notes', function(req, res){
-    res.sendFile(path.join(__dirname, '../db/db.json'))
+    res.json(getNotes())
 })
+
 // .catch((err)=> res.status(500).json(err));
-router.post('/notes', function(req, res){
-    let saveNotes = JSON.parse(readFileSync('./../db/db.json', 'utf8'));
-    let newNote = req.body;
-    let id = saveNotes.length.toString();
-    newNote.id = id;
-    saveNotes.push(newNote);
-    fs.writeFileSynce('../db/db.json', JSON.stringify(saveNotes));
-    res.json(saveNotes);
-})
+// router.post('/notes', function(req, res){
+//     let saveNotes = JSON.parse(readFileSync('./../db/db.json', 'utf8'));
+//     let newNote = req.body;
+//     let id = saveNotes.length.toString();
+//     newNote.id = id;
+//     saveNotes.push(newNote);
+//     fs.writeFileSynce('../db/db.json', JSON.stringify(saveNotes));
+//     res.json(saveNotes);
+// })
 
 module.exports = router
 
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiroutes')
 const HTMLroutes = require('./routes/htmlroutes')
+const path = require('path')
 
 
 const app = express();
-const PORT = process.env.PORT || 3000 
-
-// app.use(express.json())
-
-// app.use(express.urlencoded({extended:true}))
-
-// app.use(express.static('public'))
+const PORT = process.env.PORT || 3001 
 
 app.use('/api', apiRoutes)
 
@@ -18,31 +13,12 @@ app.use('/', HTMLroutes)
 
 // app.listen(PORT, ()=> console.log(`listening on PORT: ${PORT}`))
 
-
-
-const path = require('path');
-
-// const { clog } = require('./middleware/clog');
-// const api = require('./routes/index.js');
-// Import custom middleware, "cLog"
-// app.use(clog);
-// Middleware for parsing JSON and urlencoded form data
+// const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+
 app.use(express.static('public'));
-// GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
-// // GET Route for feedback page
-// app.get('/feedback', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-// );
-// // Wildcard route to direct users to a 404 page
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-// );
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} :rocket:`)
 );
