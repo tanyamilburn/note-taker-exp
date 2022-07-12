3bc80db9695282db8d53a5be15977417bdb323f3
Tanya Milburn <tanyamilburn89@gmail.com>
2022-07-11 20:59:15 -0600

    render notes to page

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 function getNotes(){
     const data= fs.readFileSync(path.join(__dirname,"../db/db.json"), "utf-8");
-    const parsedData = JSON.parse(data).notes;
+    const parsedData = JSON.parse(data);
     console.log("parsedData", parsedData)
     return parsedData
 }
@@ -14,12 +14,12 @@ router.get('/notes', function(req, res){
 })
 
 router.post('/notes', function(req, res){
-    let saveNotes = JSON.parse(readFileSync('./../db/db.json', 'utf-8'));
+    let saveNotes = JSON.parse(fs.readFileSync(__dirname,"../db/db.json", 'utf-8'));
     let newNote = req.body;
     let id = saveNotes.length.toString();
     newNote.id = id;
     saveNotes.push(newNote);
-    fs.writeFileSynce('../db/db.json', JSON.stringify(saveNotes));
+    fs.writeFileSynce(__dirname,"../db/db.json", JSON.stringify(saveNotes));
     res.json(saveNotes);
 })
 
@@ -28,4 +28,6 @@ module.exports = router
 // build out routes to save --get route, post route, save route
 // const store = require(), store file needs to go in db file, store .js
 // store.js: class, json parsing, and stringifying to get data in database
-// DONT CHANGE INDEX.JS file!!!
+
+
+
